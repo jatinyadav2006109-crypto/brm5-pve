7b85d405f62ca960c13eb6dff023810b107c133a
jatinyadav2006109-crypto <jatinyadav2006109@gmail.com>
2026-06-23 01:34:09 +0530

    Create services.lua

---
+++ brm5-pve/modules/services.lua
@@ -0,0 +1,11 @@
+local Services = {
+    Players = game:GetService("Players"),
+    Workspace = game:GetService("Workspace"),
+    Lighting = game:GetService("Lighting"),
+    RunService = game:GetService("RunService"),
+    UserInputService = game:GetService("UserInputService"),
+    ReplicatedStorage = game:GetService("ReplicatedStorage"),
+    localPlayer = game:GetService("Players").LocalPlayer,
+    camera = workspace.CurrentCamera
+}
+return Services
